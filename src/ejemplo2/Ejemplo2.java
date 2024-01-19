420d119eea9e5b91a439435bf2161b07cd521522
jarbi velasquez <progra@gmail.com>
2024-01-19 11:17:31 -0600

    primer repositorio

---
+++ src/ejemplo2/Ejemplo2.java
@@ -0,0 +1,16 @@
+//Jarbi velasquez
+package ejemplo2;
+
+
+public class Ejemplo2 {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Jarbi  ");
+        System.out.println("Anderson  ");
+        System.out.println("Velasquez  ");
+        System.out.println(" tuche ");
+        
+    }
+    
+}
